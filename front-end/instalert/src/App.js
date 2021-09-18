278ebab2cc31d39ffb241fd1a2e51eebabc31aa7
rcheng15jhu <rcheng15@jhu.edu>
2021-09-18 18:29:05 -0400

    Changed App to functional component and introduced map

--- front-end/instalert/src/App.js
+++ front-end/instalert/src/App.js
@@ -3,18 +3,33 @@ import './App.css';
 import Header from './components/Header/Header';
 import Post from './components/Post/Post';
 
-class App extends Component {
-    render() {
-        return <div className="App">
-            <Header />
-            <section className="App-main">
-                <Post nickname="CatLover293" avatar="http://placekitten.com/200/300" caption="OMG LOOK AT HIM HE IS SO CUTE!!!" image="http://placekitten.com/g/200/200" />
-                <Post nickname="julia_aimes" avatar="https://i.pinimg.com/originals/65/56/e9/6556e9d201517acb93cb1280832a7dab.jpg" caption="My kitten is better than yours. Fight me." image="http://placekitten.com/g/200/150" />
-
-                {/* more posts */}
-            </section>
-        </div>;
+const posts = [
+    {
+        nickname: "CatLover293",
+        avatar:   "http://placekitten.com/200/300",
+        caption:  "OMG LOOK AT HIM HE IS SO CUTE!!!",
+        image:    "http://placekitten.com/g/200/200"
+    },
+    {
+        nickname: "julia_aimes",
+        avatar:   "https://i.pinimg.com/originals/65/56/e9/6556e9d201517acb93cb1280832a7dab.jpg",
+        caption:  "My kitten is better than yours. Fight me.",
+        image:    "http://placekitten.com/g/200/150"
     }
-}
+];
+
+const App = () => {
+
+    return (
+        <div className="App">
+        <Header />
+        <section className="App-main">
+        {posts.map(post => (
+            <Post {...post}/>
+        ))}
+        </section>
+    </div>
+    )
+};
 
 export default App;
