3e6c406b3fa6cbc0ca0122754bd29223be89465f
rcheng15jhu <rcheng15@jhu.edu>
2021-09-18 18:33:05 -0400

    Change to functional style

--- front-end/instalert/src/components/Header/Header.js
+++ front-end/instalert/src/components/Header/Header.js
@@ -2,22 +2,22 @@ import React from "react";
 import "./Header.css";
 import pfp from "../../images/pfp.png";
 
-class Header extends React.Component{
-    render(){
-        return (
-            <nav className="Nav">
-                <div className="Nav-menus">
-                    <div className="Nav-brand">
-                        <h3 className="Nav-brand-logo" href="/">
-                            Instalert
-                        </h3>
-                    </div>
-                    <div>
-                        <img className="Nav-pfp" src={pfp} alt="Paris" />
-                    </div>
+const Header = () => {
+
+    return (
+        <nav className="Nav">
+            <div className="Nav-menus">
+                <div className="Nav-brand">
+                    <h3 className="Nav-brand-logo">
+                        Instalert
+                    </h3>
                 </div>
-            </nav>
-        );
-    }
-}
+                <div>
+                    <img className="Nav-pfp" src={pfp} alt="Paris" />
+                </div>
+            </div>
+        </nav>
+    );
+};
+
 export default Header;
--- front-end/instalert/src/components/Post/Post.js
+++ front-end/instalert/src/components/Post/Post.js
@@ -2,41 +2,34 @@ import React, { Component } from "react";
 import "./Post.css";
 import meat from "../../images/meatball_menu.png";
 
-class Post extends Component {
-    constructor(props){
-        super(props);
-    }
-    render() {
-        const nickname = this.props.nickname;
-        const avatar = this.props.avatar;
-        const image = this.props.image;
-        const caption = this.props.caption;
+function Post({nickname, avatar, image, caption}) {
 
-        return <article className="Post" ref="Post">
-            <header>
-                <div className="Post-user">
-                    <div className="Post-user-avatar">
-                        <img src={avatar} />
-                    </div>
-                    <div className="Post-user-nickname">
-                        <span>{nickname}</span>
-                    </div>
-                    <div className="Post-user-meatball">
-                        <img className="Meatball-menu" src={meat}/>
-                    </div>
+    return (
+        <article className="Post" ref="Post">
+        <header>
+            <div className="Post-user">
+                <div className="Post-user-avatar">
+                    <img src={avatar}  alt="avatar"/>
                 </div>
-            </header>
-            <div className="Post-image">
-                <div className="Post-image-bg">
-                    <img src={image} />
+                <div className="Post-user-nickname">
+                    <span>{nickname}</span>
+                </div>
+                <div className="Post-user-meatball">
+                    <img className="Meatball-menu" src={meat} alt="menu"/>
                 </div>
             </div>
-            <div className="Post-interactions">
+        </header>
+        <div className="Post-image">
+            <div className="Post-image-bg">
+                <img src={image} alt="Post"/>
             </div>
-            <div className="Post-caption">
-                <strong>{nickname}</strong> {caption}
-            </div>
-        </article>;
-    }
+        </div>
+        <div className="Post-interactions">
+        </div>
+        <div className="Post-caption">
+            <strong>{nickname}</strong> {caption}
+        </div>
+    </article>
+    )
 }
 export default Post;
