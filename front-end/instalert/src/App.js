c26b19bc0d4dafb4e0847d262b349e8103dbabb9
rcheng15jhu <rcheng15@jhu.edu>
2021-09-18 18:58:43 -0400

    minor import changes

--- front-end/instalert/src/App.js
+++ front-end/instalert/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Post from './components/Post/Post';
--- front-end/instalert/src/components/Post/Post.js
+++ front-end/instalert/src/components/Post/Post.js
@@ -1,35 +1,38 @@
-import React, { Component } from "react";
+import React, {useRef} from "react";
 import "./Post.css";
 import meat from "../../images/meatball_menu.png";
 
 function Post({nickname, avatar, image, caption}) {
 
+    const postRef = useRef();
+
     return (
-        <article className="Post" ref="Post">
-        <header>
-            <div className="Post-user">
-                <div className="Post-user-avatar">
-                    <img src={avatar}  alt="avatar"/>
+        <article className="Post" ref={postRef}>
+            <header>
+                <div className="Post-user">
+                    <div className="Post-user-avatar">
+                        <img src={avatar}  alt="avatar"/>
+                    </div>
+                    <div className="Post-user-nickname">
+                        <span>{nickname}</span>
+                    </div>
+                    <div className="Post-user-meatball">
+                        <img className="Meatball-menu" src={meat} alt="menu"/>
+                    </div>
                 </div>
-                <div className="Post-user-nickname">
-                    <span>{nickname}</span>
-                </div>
-                <div className="Post-user-meatball">
-                    <img className="Meatball-menu" src={meat} alt="menu"/>
+            </header>
+            <div className="Post-image">
+                <div className="Post-image-bg">
+                    <img src={image} alt="Post"/>
                 </div>
             </div>
-        </header>
-        <div className="Post-image">
-            <div className="Post-image-bg">
-                <img src={image} alt="Post"/>
+            <div className="Post-interactions">
+
             </div>
-        </div>
-        <div className="Post-interactions">
-        </div>
-        <div className="Post-caption">
-            <strong>{nickname}</strong> {caption}
-        </div>
-    </article>
+            <div className="Post-caption">
+                <strong>{nickname}</strong> {caption}
+            </div>
+        </article>
     )
 }
 export default Post;
