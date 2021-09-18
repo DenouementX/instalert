3da5b7d35632f2068212935da8d31ff1056f08b7
shintyl <tylerjshin@gmail.com>
2021-09-18 03:55:20 +0000

    add workflow for dev github action

--- app/src/main/java/instalert/Server.java
+++ app/src/main/java/instalert/Server.java
@@ -3,12 +3,12 @@
  */
 package instalert;
 
-public class App {
+public class Server {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new Server().getGreeting());
     }
 }
