c5def2516cc8a7c63633b571dea839d822c34cb9
Everett X <exu6@jh.edu>
2021-09-18 17:40:31 -0500

    Removed unnecessary import

--- front-end/instalert/src/components/SettingsCard/SettingsCard.js
+++ front-end/instalert/src/components/SettingsCard/SettingsCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import "./SettingsCard.css";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
